44c51071b8d1344ba7d93e90954f2a4c160e5b80
fymty <ryzhalexwork@gmail.com>
2019-09-20 20:49:53 +0300

    First

---
+++ cmake-build-debug/CMakeFiles/42_Push_swap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/42_Push_swap.dir/main.c.o"
+  "42_Push_swap.pdb"
+  "42_Push_swap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/42_Push_swap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
